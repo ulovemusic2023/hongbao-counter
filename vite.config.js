3a366564578398bb1d575160b9ff1416d75bc341
Gary Hsieh <garyhsieh@ulovemusic.com>
2026-02-16 18:33:19 +0800

    fix: add base path to vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 export default defineConfig({
+    base: "/hongbao-counter/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
